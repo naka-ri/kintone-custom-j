d5cd1d855f5102fde9ab9f932699dde851be5449
naka-ri <r.nakamura@tryan.co.jp>
2025-06-05 16:50:23 +0900

    Update set_recipient.js

--- set_recipient.js
+++ set_recipient.js
@@ -1,8 +1,7 @@
-(() => {
+(function () {
   "use strict";
 
-  // Boost! Injector で送信された時のレコード作成イベント
-  kb.event.on('kb.record.create.submit', (event) => {
+  kintone.events.on('app.record.create.submit', function (event) {
     event.record['送信先'].value = 'r.nakamura@tryan.co.jp';
     return event;
   });
